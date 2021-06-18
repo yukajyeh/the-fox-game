991476f40f6b1080fdd22e1c8b0129b385553aca
Yuka Yeh <yukaka210@gmail.com>
2021-06-18 13:40:33 +0200

    user-button interation

--- src/init.js
+++ src/init.js
@@ -1,19 +1,18 @@
-const TICK_RATE = 3000;
+import gameState from './gameState';
+import { TICK_RATE } from './constants';
+import initButtons from './buttons';
 
-function tick() {
-  console.log("tick", Date.now());
-}
 
 async function init() {
   console.log("starting game");
-
+  initButtons(gameState.handleUserAction);
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame(){
     const now = Date.now();
 
     if(nextTimeToTick <= now) {
-      tick()
+      gameState.tick();
       nextTimeToTick = now + TICK_RATE;
     }
 
